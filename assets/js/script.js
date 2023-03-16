440e4eb1248c8c98071a3d8bc424835559bdcb4e
Malte Mosbach <maltemosbach@gmail.com>
2023-03-16 16:55:22 +0100

    Initialize project page.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let papertitle = "Learning Generalizable Tool Use with Non-rigid Grasp-pose Registration"
+
+// set papertitle
+document.getElementById("papertitle_website").innerHTML = papertitle;
+document.getElementById("papertitle_heading").innerHTML = papertitle;
